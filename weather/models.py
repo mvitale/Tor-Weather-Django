6300db4f14ced783753136077b8b8dca89c776bb
Michael Vitale <mmvitale@gmail.com>
2010-06-09 16:42:23 -0400

    added Subscriber and started Router model

--- weather/models.py
+++ weather/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+import datetime.date
 
 # Create your models here.
+class Subscriber(models.Model)
+    email = models.EmailField(max_length=75)
+    router_id = models.ForeignKey(Router)
+
+    #change this when more is known
+    subs_auth = models.CharField(max_length=300) 
+    unsubs_auth = models.CharField(max_length=300)
+    pref_auth = models.CharField(max_length=300)
+
+    sub_date = models.DateField()
+
+class Router(models.Model)
+    finger = models
