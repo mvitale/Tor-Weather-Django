880c424f28dadb8f6a37322a27ecca4f6c754e48
Katie Wagner <kwagner@wesleyan.edu>
2010-06-10 10:32:41 -0400

    Started the Subscription and Router models

--- weather/models.py
+++ weather/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Subscription(models.Model):
+    subscriber_id = models.ForeignKey(Subscriber)
+    name = models.CharField(max_length=200)
+    threshold = models.CharField(max_length=200)
+    grace_pd = models.IntegerField()
+    emailed = models.BooleanField()
+    triggered = models.BooleanField()
+    last_changed = models.DateTimeField('date of last change')
+
+    def __unicode__(self):
+        return self.name
+
+class Router(models.Model):
+    fingerprint = models.CharField(max_length=200)
+    welcomed = models.BooleanField()
+    last_seen = models.DateTimeField('date last seen')
