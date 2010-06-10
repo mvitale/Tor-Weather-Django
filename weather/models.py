79a634d47cd56223e9fe754907187f22ac944664
jruberg <jrubegr@wesleyan.edu>
2010-06-10 13:41:07 -0400

    Updated models

--- weather/models.py
+++ weather/models.py
@@ -1,6 +1,21 @@
 from django.db import models
 import datetime.date
 
+class Subscriber(models.Model)
+    email = models.EmailField(max_length=75)
+    router_id = models.ForeignKey(Router)
+    confirmed = models.BooleanField()
+
+    #change this when more is known
+    subs_auth = models.CharField(max_length=250) 
+    unsubs_auth = models.CharField(max_length=250)
+    pref_auth = models.CharField(max_length=250)
+
+    sub_date = models.DateField()
+
+    def __unicode__(self):
+        return self.email
+
 class Subscription(models.Model):
     subscriber_id = models.ForeignKey(Subscriber)
     name = models.CharField(max_length=200)
@@ -17,14 +32,8 @@ class Router(models.Model):
     fingerprint = models.CharField(max_length=200)
     welcomed = models.BooleanField()
     last_seen = models.DateTimeField('date last seen')
+    
+    def __unicode__(self):
+        return self.fringerprint
 
-class Subscriber(models.Model)
-    email = models.EmailField(max_length=75)
-    router_id = models.ForeignKey(Router)
 
-    #change this when more is known
-    subs_auth = models.CharField(max_length=250) 
-    unsubs_auth = models.CharField(max_length=250)
-    pref_auth = models.CharField(max_length=250)
-
-    sub_date = models.DateField()
