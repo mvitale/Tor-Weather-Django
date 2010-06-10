19e74cb01d9c4f198ed712b0049764fbf3864d19
Michael Vitale <mmvitale@gmail.com>
2010-06-10 14:19:14 -0400

    'Created an Emailer class for sending emails.'

--- weather/models.py
+++ weather/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 import datetime.date
+from django.core.mail import send_mail
 
 class Subscription(models.Model):
     subscriber_id = models.ForeignKey(Subscriber)
@@ -28,3 +29,23 @@ class Subscriber(models.Model)
     pref_auth = models.CharField(max_length=250)
 
     sub_date = models.DateField()
+
+class Emailer(models.Model):
+    """A class for sending email messages"""
+    def sendEmail(sender, recipient, messageText, subject):
+        """
+        Send an email with message messageText and subject subject to
+        recipient from sender
+        
+        @type sender: string
+        @param sender: The sender of this email.
+        @type recipient: string
+        @param recipient: The recipient of this email.
+        @type messageText: string
+        @param messageText: The contents of this email.
+        @type subject: string
+        @param subject: The subject of this email.
+        """
+        to = [recipient] #send_mail takes a list of recipients
+        send_mail(subject, messageText, sender, recipient, fail_silently=True)
+
