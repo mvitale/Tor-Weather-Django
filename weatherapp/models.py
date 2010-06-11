9554a992a980e91db24007454ca5af9996b7c549
jruberg <jrubegr@wesleyan.edu>
2010-06-11 16:12:34 -0400

    'Added random string generator to models, put in form checking to the subscribe view (not sure if it's correct yet), and added a add_new_subscriber class method to the Subscriber model'

--- weatherapp/models.py
+++ weatherapp/models.py
@@ -4,6 +4,13 @@ import datetime
 #import TorCtl.TorCtl
 import socket
 #import weather.config
+from weather.weatherapps import emails
+import base64
+
+# Supposedly required to make class methods
+class Callable:
+    def __init__(self, anycallable):
+        self.__call__ = anycallable
 
 class Router(models.Model):
     fingerprint = models.CharField(max_length=200)
@@ -16,7 +23,7 @@ class Router(models.Model):
 
 class Subscriber(models.Model):
     email = models.EmailField(max_length=75)
-    router_id = models.ForeignKey(Router)
+    router = models.ForeignKey(Router)
     confirmed = models.BooleanField()
 
     #change this when more is known?
@@ -29,8 +36,31 @@ class Subscriber(models.Model):
     def __unicode__(self):
         return self.email
 
+    def add_new_subscriber(email, router_id, confirmed=False,
+            confirm_auth="", unsubs_auth="", pref_auth="",
+            sub_date=datetime.datetime.now()):
+        
+        g = StringGenerator()
+        if confirm_auth == "":
+            confirm_auth = g.get_rand_string()
+        if unsubs_auth == "":
+            unsubs_auth = g.get_rand_string()
+        if pref_auth == "":
+            pref_auth = g.get_rand_string()
+        
+        subscriber = Subscriber(email = email, router = router_id,
+                confirmed = confirmed, confirm_auth = confirm_auth,
+                unsubs_auth = unsubs_auth, pref_auth = pref_auth, 
+                sub_date = sub_date)
+        subscriber.save()
+        return subscriber
+    
+    # supposedly makes add_new_subscriber() a class method
+    add_new_subscriber = Callable(add_new_subscriber)
+        
+
 class Subscription(models.Model):
-    subscriber_id = models.ForeignKey(Subscriber)
+    subscriber = models.ForeignKey(Subscriber)
     name = models.CharField(max_length=200)
     threshold = models.CharField(max_length=200)
     grace_pd = models.IntegerField()
@@ -41,21 +71,81 @@ class Subscription(models.Model):
     def __unicode__(self):
         return self.name
 
-#class Emailer(models.Model):
-#    """A class for sending email messages"""
-#    def sendEmail(sender, recipient, messageText, subject):
-#        """
-#        Send an email with message messageText and subject subject to
-#        recipient from sender
-#        
-#        @type sender: string
-#        @param sender: The sender of this email.
-#        @type recipient: string
-#        @param recipient: The recipient of this email.
-#        @type messageText: string
-#        @param messageText: The contents of this email.
-#        @type subject: string
-#        @param subject: The subject of this email.
-#        """
-#        to = [recipient] #send_mail takes a list of recipients
-#        send_mail(subject, messageText, sender, recipient, fail_silently=True)
+class Emailer(models.Model):
+    """A class for sending email messages"""
+    
+    def send_generic_mail(recipient, subject, messageText, 
+                  sender = 'tor-ops@torproject.org'):
+        """
+        Send an email to single recipient recipient with subject subject and
+        message messageText, from sender with default value 
+        tor-ops@torproject.org.
+        
+        @type recipient: string
+        @param recipient: The recipient of this email.
+        @type subject: string
+        @param subject: The subject of this email.
+        @type messageText: string
+        @param messageText: The content of this email.
+        @type sender: string
+        @param sender: The sender of this email. Default value of 
+                       'tor-ops@torporject.org'.
+        """
+
+        to = [recipient] #send_mail takes a list of recipients
+        send_mail(subject, messageText, sender, to, fail_silently=True)
+
+    def send_confirmation(recipient, 
+            subject = '[Tor Weather] Confirmation Needed', 
+            messageText = emails.CONFIRMATION_MAIL):
+        """
+        Send a confirmation email to recipient recipient, with default
+        subject '[Tor Weather] Confirmation Needed' and default content
+        defined in emails.py.
+
+        @type recipient: string
+        @param recipient: The recipient of this email.
+        @type subject: string
+        @param subject: The subject of this email. Default value of '[Tor 
+                        Weather] Confirmation Needed'.
+        @type messageText: string
+        @param messageText: The content of this email. Default value of 
+                            emails.CONFIRMATION_MAIL.
+        """
+
+        send_generic_mail(recipient, subject, messageText)
+
+    def send_confirmed_email(recipient,
+            subject = '[Tor Weather] Confirmation Successful',
+            messageText = emails.SUBS_CONFIRMED_MAIL):
+
+        send_generic_mail(recipient, subject, messageText)
+
+    def send_node_down_email(recipient,
+            subject = '[Tor Weather] Node Down!',
+            messageText = emails.NODE_DOWN_MAIL):
+
+        send_generic_mail(recipient, subject, messageText)
+
+    def send_out_of_date_email(recipient,
+            subject = '[Tor Weather] Node Out of Date!',
+            messageText = emails.OUT_OF_DATE_MAIL):
+
+        send_generic_mail(recipient, subject, messageText)
+
+    def send_t_shirt_email(recipient,
+            subject = '[Tor Weather] Congratulations! Have a shirt!',
+            messageText = emails.T_SHIRT_MAIL):
+        
+        send_generic_mail(recipient, subject, messageText)
+
+class StringGenerator:
+    def get_rand_string():
+        # Code pulled from original Weather, not sure why it cuts off
+        # the last character
+        r = base64.urlsafe_b64encode(os.urandom(18))[:-1]
+
+        # some email clients don't like URLs ending in -
+        if r.endswith("-"):
+            r = r.replace("-", "x")
+        return r
--- weatherapp/views.py
+++ weatherapp/views.py
@@ -1,23 +1,47 @@
 from django.shortcuts import render_to_response, get_object_or_404
-from weather.weatherapp.models import Subscriber
+from weather.weatherapp.models import Subscriber, Subscription
+from django.core.mail import send_mail 
+from weather.weatherapp.emails
+from weather.weatherapp.models import Emailer
 
 # -----------------------------------------------------------------------
-#FILL IN ONCE WE KNOW THE SITE! 
+# FILL IN ONCE WE KNOW THE SITE! ----------------------------------------
 # -----------------------------------------------------------------------
 baseURL = "localhost:8000"
 
 def subscribe(request):
-    return render_to_response('subscribe.html')
+    if request.method == 'POST'
+        form = SubscribeForm(request.POST)
 
-def pending(request):
-    sub = get_object_or_404(Subscriber, pk=subscriber_id)
+# -----------------------------------------------------------------------
+# NEED TO CHECK HOW DJANGO CHECKS IF AN EMAIL FIELD IS VALID ------------
+# -----------------------------------------------------------------------
+        if form.is_valid():
+            addr = form.cleaned_data['email']
+            fingerprint = form.cleaned_data['router_id']
+            grace_pd = form.cleaned_data['grace_pd']
+            
+            Emailer.send_confirmation_mail(addr)
+
+            # Add subscriber to the database
+            subscriber = Subscriber.add_new_subscriber() 
+
+            
+            return HttpResponseRedirect('/pending/'+subscriber.id+'/')
+    else:
+        form = SubscribeForm()
+    return render_to_response('subscribe.html', {'form': form,})
+
+def pending(request, subscriber_id):
+    suber = get_object_or_404(Subscriber, pk=subscriber_id)
     return render_to_response('pending.html', {'email': sub.email})
 
 def confirm(request, confirm_auth_id):
+    subscriber = get_object
     sub = get_object_or_404(Subscriber, confirm_auth=confirm_auth_id)
     rout = Router.objects.get(pk=sub.router_id)
-    unsubURL = baseURL + "/unsubscribe/" + sub.unsubs_auth + "/"
-    prefURL = baseURL + "/preferences/" + sub.pref_auth + "/"
+    unsubURL = baseURL + "/unsubscribe/" + suber.unsubs_auth + "/"
+    prefURL = baseURL + "/preferences/" + suber.pref_auth + "/"
     return render_to_response('confirm.html', {'email': sub.email, \
             'fingerprint' : rout.fingerprint, 'nodeName' : rout.name, \
             'unsubURL' : unsubURL, 'prefURL' : prefURL})
