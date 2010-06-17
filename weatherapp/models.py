ee012f21cd4ea112ee6c3373b22695146c6b22a5
jruberg <jrubegr@wesleyan.edu>
2010-06-17 12:09:14 -0400

    organized models better and actually incorporated handy django stuff

--- weatherapp/models.py
+++ weatherapp/models.py
@@ -14,21 +14,16 @@ from weatherapp.helpers import Emailer
 from datetime import datetime
 import base64
 
+_WELCOMED_DEFAULT = False
+_UP_DEFAULT = True
+
 class RouterManager(models.Manager):
-    _WELCOMED_DEFAULT = False
-
-    def add_default_router(self, fingerprint, name,
-                           welcomed = None,
-                           last_seen = None):
-        if welcomed == None:
-            welcomed = _WELCOMED_DEFAULT
-        if last_seen == None:
-            last_seen = datetime.now()
-
-        routr = Router(fingerprint = fingerprint, name = name,
-                       welcomed = welcomed, last_seen = last_seen)
-        routr.save()
-        return routr
+    def is_in(self, fingerprint = None,
+              name = None,
+              welcomed = None,
+              last_seen = None):
+        # TO DO -------------------------------------------------- BASE FEATURE
+        # PUT CODE HERE -------------------------------------------------------
 
     def get_query_set(self):
         return super(RouterManager, self).get_query_set()
@@ -44,80 +39,81 @@ class Router(models.Model):
     @ivar name: The name associated with the router.
     @type welcomed: bool
     @ivar welcomed: true if the router operater has received a welcome email,
-                    false if they haven't.
+                    false if they haven't. Default value is C{False}.
     @type last_seen: datetime
     @ivar last_seen: The most recent time the router was listed on a consensus 
-                     document. In the view that is processed when a consensus
-                     document is received, will store the datetime when the
-                     consensus document was received and so will be able to
-                     check if the last_seen datetime matches with the consensus
-                     datetime, informing whether the router is up.
+                     document. Default value is C{datetime.now()}.
     @type up: bool
     @ivar up: True if this router was up last time a new network consensus
-              was published, false otherwise.
+              was published, false otherwise. Default value is C{True}.
     """
 
-    fingerprint = models.CharField(max_length=200)
+    fingerprint = models.CharField(max_length=200, unique=True)
     name = models.CharField(max_length=100)
-    welcomed = models.BooleanField()
-    last_seen = models.DateTimeField('date last seen')
-    up = models.BooleanField()
+    welcomed = models.BooleanField(default=False)
+    last_seen = models.DateTimeField('date last seen', default=datetime.now())
+    up = models.BooleanField(default=True)
 
     objects = RouterManager()
     
     def __unicode__(self):
         return self.fingerprint
 
-    def update(self, 
-               fingerprint = None,
-               name = None,
-               welcomed = None,
-               last_seen = None,
-               up = None)
-        if fingerprint != None:
-            self.fingerprint = fingerprint
-        if name != None:
-            self.name = name
-        if welcomed != None:
-            self.welcomed = welcomed
-        if last_seen != None:
-            self.last_seen = last_seen
-        if up != None:
-            self.up = up
-
-        self.save()
-        return self
-
 class SubscriberManager(models.Manager):
-    _CONFIRMED_DEFAULT = False
+   def is_in(self,
+             email = None,
+             router = None,
+             confirmed = None,
+             confirm_auth = None,
+             unsubs_auth = None,
+             pref_auth = None,
+             sub_date = None):
+           
+        # TO DO -------------------------------------------------- BASE FEATURE
+        # PUT CODE HERE -------------------------------------------------------
 
-    def add_default_subscriber(self, email, router_id,
-                               confirmed = None,
-                               confirm_auth = None,
-                               unsubs_auth = None,
-                               pref_auth = None,
-                               sub_date = None):
-        if confirmed == None:
-            confirmed = _CONFIRMED_DEFAULT
-
-        if confirm_auth == None:
-            confirm_auth = get_rand_string()
-        if unsubs_auth == None:
-            unsubs_auth = get_rand_string()
-        if pref_auth == None:
-            pref_auth = get_rand_string()
-
-        if sub_date == None:
-            sub_date = datetime.now()
-
-        subr = Subscriber(email = email, router = router_id,
-                          confirmed = confirmed, confirm_auth = confirm_auth,
-                          unsubs_auth = unsubs_auth, pref_auth = pref_auth)
-        subr.save()
-        return subr
-
-    def get_query_set(self):
+   def get_query_set(self):
         return super(SubscriberManager, self).get_query_set()
+
+class Subscriber(models.Model):
+    """
+    A model to store information about Tor Weather subscribers, including their
+    authorization keys.
+
+    @ivar email: The subscriber's email.
+    @ivar router: A foreign key link to the router model corresponding to the
+        node this subscriber is watching.
+    @type confirmed: bool
+    @ivar confirmed: True if the subscriber has confirmed the subscription by
+        following the link in their confirmation email and False otherwise. 
+        Default value upon creation is C{False}.
+    @type confirm_auth: str
+    @ivar confirm_auth: This user's confirmation key, which is incorporated into
+        the confirmation url.
+    @type unsubs_auth: str
+    @ivar unsubs_auth: This user's unsubscribe key, which is incorporated into 
+        the unsubscribe url.
+    @type pref_auth: str
+    @ivar pref_auth: The key for this user's Tor Weather preferences page.
+    @type sub_date: datetime.datetime
+    @ivar sub_date: The date this user subscribed to Tor Weather. Default value
+                    upon creation is datetime.now().
+    """
+    email = models.EmailField(max_length=75)
+    router = models.ForeignKey(Router)
+    confirmed = models.BooleanField(default = False)
+
+    #change this when more is known?
+    confirm_auth = models.CharField(max_length=250, default=get_rand_string()) 
+    unsubs_auth = models.CharField(max_length=250, default=get_rand_string())
+    pref_auth = models.CharField(max_length=250, default=get_rand_string())
+
+    sub_date = models.DateTimeField(default=datetime.now())
+
+    objects = SubscriberManager()
+
+    def __unicode__(self):
+        return self.email
 
     def get_rand_string(length = 24):
         cut_off = length - 24
@@ -131,88 +127,20 @@ class SubscriberManager(models.Manager):
             r = r.replace("-", "x")
         return r
 
-
-class Subscriber(models.Model):
-    """
-    A model to store information about Tor Weather subscribers, including their
-    authorization keys.
-
-    @ivar email: The subscriber's email.
-    @ivar router: A foreign key link to the router model corresponding to the
-        node this subscriber is watching.
-    @type confirmed: bool
-    @ivar confirmed: True if the subscriber has confirmed the subscription by
-        following the link in their confirmation email and False otherwise.
-    @type confirm_auth: str
-    @ivar confirm_auth: This user's confirmation key, which is incorporated into
-        the confirmation url.
-    @type unsubs_auth: str
-    @ivar unsubs_auth: This user's unsubscribe key, which is incorporated into 
-        the unsubscribe url.
-    @type pref_auth: str
-    @ivar pref_auth: The key for this user's Tor Weather preferences page.
-    @type sub_date: datetime.datetime
-    @ivar sub_date: The date this user subscribed to Tor Weather.
-    """
-    email = models.EmailField(max_length=75)
-    router = models.ForeignKey(Router)
-    confirmed = models.BooleanField()
-
-    #change this when more is known?
-    confirm_auth = models.CharField(max_length=250) 
-    unsubs_auth = models.CharField(max_length=250)
-    pref_auth = models.CharField(max_length=250)
-
-    sub_date = models.DateTimeField()
-
-    objects = SubscriberManager()
-
-    def __unicode__(self):
-        return self.email
-
-    def update(self,
-               email = None,
-               router = None,
-               confirmed = None,
-               confirm_auth = None,
-               unsubs_auth = None,
-               pref_auth = None,
-               sub_date = None):
-        if email != None:
-            self.email = email
-        if router != None:
-            self.router = router
-        if confirmed != None:
-            self.confirmed = confirmed
-        if confirm_auth != None:
-            self.confirm_auth = confirm_auth
-        if unsubs_auth != None:
-            self.unsubs_auth = unsubs_auth
-        if pref_auth != None:
-            self.pref_auth = pref_auth
-        if sub_date != None:
-            self.sub_date = sub_date
-
-        self.save()
-        return self
-
 class SubscriptionManager(models.Manager):
-    _EMAILED_DEFAULT = False
-    _TRIGGERED_DEFAULT = False
+   def is_in(self, 
+             subscriber = None,
+             name = None,
+             threshold = None,
+             grace_pd = None,
+             emailed = None,
+             triggered = None,
+             last_changed = None):
+             
+        # TO DO -------------------------------------------------- BASE FEATURE
+        # PUT CODE HERE -------------------------------------------------------
 
-    def add_default_subscription(self, subscriber_id, name, threshold, 
-                                 grace_pd,
-                                 emailed = None,
-                                 triggered = None,
-                                 last_changed = None):
-        if emailed == None:
-            emailed = _EMAILED_DEFAULT
-        if triggered == None,
-            triggered = _TRIGGERED_DEFAULT
-        if last_changed == None,
-            last_changed = datetime.now()
-            
-    def get_query_set(self):
+   def get_query_set(self):
         return super(SubscriptionManager, self).get_query_set()
 
 class Subscription(models.Model):
@@ -235,22 +163,24 @@ class Subscription(models.Model):
         after a subscription type is triggered.
     @type emailed: bool
     @ivar emailed: True if the user has been emailed about the subscription
-        (trigger must also be True), False if the user has not been emailed.
+        (trigger must also be True), False if the user has not been emailed. 
+        Default upon creation is C{False}.
     @type triggered: bool
     @ivar triggered: True if the threshold has been passed for this 
         subscription/the conditions to send a notification are met, False
-        if not.
+        if not. Default upon creation is C{False}.
     @type last_changed: datetime.datetime
     @ivar last_changed: The most recent datetime when the trigger field 
-        was changed.
+        was changed. Default upon creation is C{datetime.now()}.
     """
     subscriber = models.ForeignKey(Subscriber)
     name = models.CharField(max_length=200)
     threshold = models.CharField(max_length=200)
     grace_pd = models.IntegerField()
-    emailed = models.BooleanField()
-    triggered = models.BooleanField()
-    last_changed = models.DateTimeField('date of last change')
+    emailed = models.BooleanField(default=False)
+    triggered = models.BooleanField(default=False)
+    last_changed = models.DateTimeField('date of last change', 
+                                        default=datetime.now())
 
     objects = SubscriptionManager()
     
@@ -265,32 +195,6 @@ class Subscription(models.Model):
             return True
         else:
             return False
-
-    def update(self,
-               subscriber = None,
-               name = None,
-               threshold = None,
-               grace_pd = None,
-               emailed = None,
-               triggered = None,
-               last_changed = None):
-        if subscriber == None:
-            self.subscriber = subscriber
-        if name == None:
-            self.name = name
-        if threshold == None:
-            self.threshold = threshold
-        if grace_pd == None:
-            self.grace_pd = grace_pd
-        if emailed == None:
-            self.emailed = emailed
-        if triggered = None:
-            self.triggered = triggered
-        if last_changed = None:
-            self.last_changed = last_changed
-
-        self.save()
-        return self
 
 class SubscribeForm(forms.Form):
     """The form for a new subscriber. The form includes an email field, 
