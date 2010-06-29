a8471f82fc940f6a9431118d41a786110082cf8d
Katie Wagner <kwagner@wesleyan.edu>
2010-06-29 17:00:00 -0400

    Added documentation.

--- weather/weatherapp/tests.py
+++ weather/weatherapp/tests.py
@@ -17,4 +17,10 @@ class TestWeb(TestCase):
                                           'fingerprint' : '1234', 
                                           'grace_pd' : 1})
         self.assertEqual(response.status_code, 200)
- 
+        print response.content
+    def test_subscribe_bad(self):
+        c = Client()
+        response = c.post('/subscribe/', {'email' : 'name@place.com',
+                                          'fingerprint' : '12345'})
+        self.assertEqual(response.status_code, 200)
+        print response.content
