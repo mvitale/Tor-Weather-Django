afd2b2a408719f0ec3d9ebfd741480e4c8f6e21b
jruberg <jrubegr@wesleyan.edu>
2010-06-29 11:35:10 -0400

    successfully got frontend to display/act correctly usign jquery

--- media/script.js
+++ media/script.js
@@ -1,12 +1,25 @@
+function showOrHide(check, sect) {
+	if ($(check).attr('checked')) {
+		$(sect).show();
+	} else {
+		$(sect).hide();
+	}
+}
+
 $(document).ready(function() {
-		$("#node-down-check input").click(function() {
-			$("div#node-down-section").slideToggle("fast");
-			});
-		$("#out-of-date-check input").click(function() {
-			$("div#out-of-date-section").slideToggle("fast");
-			});
-		$("#band-low-check input").click(function() {
-			$("div#band-low-section").slideToggle("fast");
-			});
-		});
+
+	showOrHide("#node-down-check input", "div#node-down-section");
+	showOrHide("#out-of-date-check input", "div#out-of-date-section");
+	showOrHide("#band-low-check input", "div#band-low-section");
+
+	$("#node-down-check input").click(function() {
+		$("div#node-down-section").slideToggle("fast");
+	});
+	$("#out-of-date-check input").click(function() {
+		$("div#out-of-date-section").slideToggle("fast");
+	});
+	$("#band-low-check input").click(function() {
+		$("div#band-low-section").slideToggle("fast");
+	});
+});
 
