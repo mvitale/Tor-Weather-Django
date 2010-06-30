1a5763f758fe8cb1f3deb6e3afeeb50ccef4ec83
Michael Vitale <mmvitale@gmail.com>
2010-06-30 13:59:57 -0400

    Added t-shirt subscription test and removed duplicate bandwidth method from ctlutil

--- weather/weatherapp/tests.py
+++ weather/weatherapp/tests.py
@@ -2,7 +2,8 @@
 The test module. To run tests, cd to weather and run 'python manage.py
 test weatherapp'.
 """
-from models import Subscriber, Subscription, Router, NodeDownSub, TShirtSub, VersionSub
+from models import Subscriber, Subscription, Router, NodeDownSub, TShirtSub,\
+                   VersionSub
 
 from django.test import TestCase
 from django.test.client import Client
@@ -102,7 +103,7 @@ class TestWeb(TestCase):
                                           'get_node_down': False,
                                           'get_out_of_date' : False,
                                           'get_band_low' : True,
-                                          'band_low_threshold' : 20,
+                                          'band_low_threshold' : 40,
                                           'band_low_grace_pd' : 2,
                                           'get_t_shirt' : False},
                                           follow = True)
@@ -118,7 +119,7 @@ class TestWeb(TestCase):
         self.assertEquals(mail.outbox[0].subject, 
                           '[Tor Weather] Confirmation Needed')
 
-        #Check if the correct info was stored
+        #Check if the correct subscriber info was stored
         subscriber = Subscriber.objects.get(email = 'name@place.com')
         self.assertEqual(subscriber.email, 'name@place.com')
         self.assertEqual(subscriber.router.fingerprint, '1234')
@@ -127,9 +128,51 @@ class TestWeb(TestCase):
         # there should only be one subscription for this subscriber
         subscription_list = Subscription.objects.filter(subscriber = subscriber)
         self.assertEqual(len(subscription_list), 1)
-        version_sub = VersionSub.objects.get(subscriber = subscriber)
-        self.assertEqual(version_sub.emailed, False)
 
+        #Verify that the subscription was stored correctly 
+        bandwidth_sub = BandwidthSub.objects.get(subscriber = subscriber)
+        self.assertEqual(bandwidth_sub.emailed, False)
+        self.assertEqual(bandwidth_sub.grace_pd, 2)
+        self.assertEqual(bandwidth_sub.triggered, False)
+        self.assertEqual(bandwidth_sub.threshold, 40)
+
+    def test_subscribe_shirt(self):
+        """Test a t-shirt only subscription attempt"""
+        c = Client()
+        response = c.post('/subscribe/', {'email_1' : 'name@place.com',
+                                          'email_2' : 'name@place.com',
+                                          'fingerprint' : '1234',
+                                          'get_node_down' : False,
+                                          'get_out_of_date' : False,
+                                          'get_band_low' : False,
+                                          'get_t_shirt' : True},
+                                          follow = True)
+
+         #We want to be redirected to the pending page
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.template[0].name, 'pending.html')
+        
+        #Test that one message has been sent
+        self.assertEquals(len(mail.outbox), 1)
+
+        #Verify that the subject of the message is correct.
+        self.assertEquals(mail.outbox[0].subject, 
+                          '[Tor Weather] Confirmation Needed')
+
+        #Check if the correct subscriber info was stored
+        subscriber = Subscriber.objects.get(email = 'name@place.com')
+        self.assertEqual(subscriber.email, 'name@place.com')
+        self.assertEqual(subscriber.router.fingerprint, '1234')
+        self.assertEqual(subscriber.confirmed, False)
+
+        # there should only be one subscription for this subscriber
+        subscription_list = Subscription.objects.filter(subscriber = subscriber)
+        self.assertEqual(len(subscription_list), 1)
+
+        #Verify that the subscription was stored correctly
+        shirt_sub = TShirtSub.objects.get(subscriber = subscriber)
+        self.assertEqual(shirt_sub.emailed, False)
+        self.assertEqual(shirt_sub.avg_bandwidth, 0)
 
     def test_subscribe_bad(self):
         c = Client()
