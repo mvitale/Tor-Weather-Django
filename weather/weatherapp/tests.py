d524b4a734e75af2989c9466994f4a3f1b1f7449
Katie Wagner <kwagner@wesleyan.edu>
2010-06-30 14:08:19 -0400

    Fixed the T-shirt updating to use bandwidth in KB/s

--- weather/weatherapp/tests.py
+++ weather/weatherapp/tests.py
@@ -2,7 +2,8 @@
 The test module. To run tests, cd to weather and run 'python manage.py
 test weatherapp'.
 """
-from models import Subscriber, Subscription, Router, NodeDownSub, VersionSub
+from models import Subscriber, Subscription, Router, NodeDownSub, TShirtSub,\
+                   VersionSub, BandwidthSub
 
 from django.test import TestCase
 from django.test.client import Client
@@ -53,8 +54,7 @@ class TestWeb(TestCase):
                                           'fingerprint' : '1234',
                                           'get_node_down' : False,
                                           'get_out_of_date' : True,
-                                          'out_of_date_threshold' : \
-                                          'c1',
+                                          'out_of_date_threshold' : 'c1',
                                           'get_band_low': False,
                                           'get_t_shirt' : False},
                                           follow = True)
@@ -90,9 +90,60 @@ class TestWeb(TestCase):
                                           'band_low_threshold' : 20,
                                           'band_low_grace_pd' : 2,
                                           'get_t_shirt' : False})
-
+        #we want to be redirected to the pending page
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.template[0].name, 'pending.html')
+
+        subscriber = Subscriber.objects.get(email = 'name@place.com')
+        self.assertEqual(subscriber.email, 'name@place.com')
+        self.assertEqual(subscriber.router.fingerprint, '1234')
+        self.assertEqual(subscriber.confirmed, False)
+
+    def test_subscribe_all(self):
+        """Test a subscribe attempt to all subscription types, relying
+        on default values."""
+        c = Client()
+        response = c.post('/subscribe/', {'email_1' : 'name@place.com',
+                                          'email_2' : 'name@place.com',
+                                          'fingerprint' : '1234',
+                                          'get_node_down' : True,
+                                          'get_out_of_date' : True,
+                                          'out_of_date_threshold' : 'c1',
+                                          'get_band_low': True,
+                                          'get_t_shirt' : True},
+                                          follow = True)
+        print response
+        #we want to be redirected to the pending page
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.template[0].name, 'pending.html')
+
+        subscriber = Subscriber.objects.get(email = 'name@place.com')
+        self.assertEqual(subscriber.email, 'name@place.com')
+        self.assertEqual(subscriber.router.fingerprint, '1234')
+        self.assertEqual(subscriber.confirmed, False)
+
+        # there should be four subscriptions for this subscriber
+        subscription_list = Subscription.objects.filter(subscriber = subscriber)
+        self.assertEqual(len(subscription_list), 4)
+        
+        node_down_sub = NodeDownSub.objects.get(subscriber = subscriber)
+        self.assertEqual(node_down_sub.emailed, False)
+        self.assertEqual(node_down_sub.triggered, False)
+        self.assertEqual(node_down_sub.grace_pd, 1)
+        
+        version = VersionSub.objects.get(subscriber = subscriber)
+        self.assertEqual(version.emailed, False)
+        #self.assertEqual(version.notify_type,
+
+        bandwidth = BandwidthSub.objects.get(subscriber = subscriber)
+        self.assertEqual(bandwidth.triggered, False)
+        self.assertEqual(bandwidth.emailed, False)
+        self.assertEqual(bandwidth.grace_pd, 1)
+        self.assertEqual(bandwidth.threshold, 20)
+        
+        tshirt = TShirtSub.objects.get(subscriber = subscriber)
+        self.assertEqual(tshirt.avg_bandwidth, 0)
+        self.assertEqual(tshirt.emailed, False)
 
     def test_subscribe_bad(self):
         c = Client()
