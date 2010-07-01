d6bc0d82f8b2cfb1f04d33252e45b8011805c943
jruberg <jrubegr@wesleyan.edu>
2010-07-01 11:59:22 -0400

    working on improving text replacement for default value display in form

--- weather/media/script.js
+++ weather/media/script.js
@@ -7,15 +7,14 @@ function showOrHide(check, sect) {
 }
 
 function showDefault(row, def_val, min_val, max_val) {
-	var val = $(row + " input").val();
-	if (!(val >= min_val && val <= max_val)) {
-		$(row + " input").val("Default value is " + def_val);
-		$(row + " input").css("color", "rgb(150, 150, 150)");
-		$(row + " input").click(function() {
-			$(row + " input").val("");
-		});
-		$(row + " input").click(function() {
-			$(row + " input").css("color", "black");
+	var val = $(row + " input[type='text']").val();
+	if (val == "Default value is " + def_val ||
+			$(row + " input[type='hidden']").val() == 'on') {
+		$(row + " input[type='text']").css("color", "rgb(150, 150, 150)");
+
+		$(row + " input[type='text']").click(function() {
+			$(row + " input[type='text']").val("");
+			$(row + " input[type='text']").css("color", "black");
 		});
 	}
 }
@@ -26,22 +25,9 @@ $(document).ready(function() {
 	showOrHide("#version-check input", "div#version-section");
 	showOrHide("#band-low-check input", "div#band-low-section");
 
+	showDefault("div#node-down-grace-pd-row", 1, 1, 4500);
 	showDefault("div#band-low-threshold-row", 20, 0, 100000);
 	showDefault("div#band-low-grace-pd-row", 1, 1, 4500);
-
-	var val = $("div#node-down-grace-pd-row input").val();
-	if (!(val >= 1 && val <= 4500)) {
-		$("div#node-down-grace-pd-row input").val("Default value is 1");
-		$("div#node-down-grace-pd-row input").css("color", "rgb(150, 150, 150)");
-		$("div#node-down-grace-pd-row input").click(function() {
-			$("div#node-down-grace-pd-row input").val("");
-		});
-		$("div#node-down-grace-pd-row input").click(function() {
-			$("div#node-down-grace-pd-row input").css("color", "black");
-		});
-	
-	}
-
 
 	$("#node-down-check input").click(function() {
 		$("div#node-down-section").toggle();
