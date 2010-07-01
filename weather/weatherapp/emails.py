1de59af24d89c1330c23688695b4aa66dd0f1ad3
Katie Wagner <kwagner@wesleyan.edu>
2010-07-01 13:35:38 -0400

    Formatted emails to contain the name of the router being observed

--- weather/weatherapp/emails.py
+++ weather/weatherapp/emails.py
@@ -53,7 +53,7 @@ _CONFIRMATION_SUBJ = 'Confirmation Needed'
 _CONFIRMATION_MAIL = "Dear human,\n\n" +\
     "This is the Tor Weather Report system.\n\n" +\
     "Someone (possibly you) has requested that status monitoring "+\
-    "information about a Tor node (id: %s) be sent to this email "+\
+    "information about a Tor node %s be sent to this email "+\
     "address.\n\nIf you wish to confirm this request, please visit the "+\
     "following url:\n\n%s\n\nIf you do not wish to receive Tor Weather "+\
     "Reports, you don't need to do anything. You shouldn't hear from us "+\
@@ -62,32 +62,30 @@ _CONFIRMATION_MAIL = "Dear human,\n\n" +\
 _CONFIRMED_SUBJ = 'Confirmation Successful'
 _CONFIRMED_MAIL="Dear human,\n\nThis is the Tor Weather Report "+\
     "system.You successfully subscribed for Weather Reports about a Tor "+\
-    "node (id: %s)\n\nYou can unsubscribe from these reports at any time "+\
+    "node %s\n\nYou can unsubscribe from these reports at any time "+\
     "by visiting the following url:\n\n%s\n\n or change your Tor Weather "+\
     "notification preferences here: \n\n%s\n"
 
 _NODE_DOWN_SUBJ = 'Node Down!'
 _NODE_DOWN_MAIL = "This is a Tor Weather Report.\n\n" +\
-    "It appears that a Tor node you elected to monitor (node id: %s) " +\
+    "It appears that the node %s that you elected to monitor " +\
     "has been uncontactable through the Tor network for at least %s "+\
     "hour(s). You may wish to look at it to see why.\n\n You can "+\
     "unsubscribe from these reports at any time by visiting the "+\
     "following url:\n\n%s\n\n or change your Tor Weather notification "+\
     "preferences here:\n\n%s\n"
-# ------------------------------------------------------------------
-# CONFIGURE THIS!
-# ------------------------------------------------------------------
+
 _VERSION_SUBJ = 'Node Out of Date!'
 _VERSION_MAIL = "This is a Tor Weather Report.\n\n"+\
-    "It appears that a Tor node you elected to monitor (node id: %s) "+\
-    "is running an out of date version of Tor. You can download the "+\
+    "It appears that a Tor node %s you elected to monitor "+\
+    "is running an %s version of Tor. You can download the "+\
     "latest version of Tor at %s.\n\n You can unsubscribe from these "+\
     "reports at any time by visiting the following url:\n\n%s\n\n"+\
     "or change your Tor Weather notification preferences here:\n\n%s\n"
 
 _LOW_BANDWIDTH_SUBJ = 'Low bandwidth!'
 _LOW_BANDWIDTH_MAIL = "The is a Tor Weather Report.\n\n"+\
-    "It appears that a tor node you elected to monitor (node id: %s) "+\
+    "It appears that a tor node %s you elected to monitor "+\
     "has had an observed bandwidth of less than 50KB/s for at least %s "+\
     "hours(s). You may wish to look at it to see why.\n\n You can "+\
     "unsubscribe from these reports at any time by visiting the "+\
@@ -96,7 +94,7 @@ _LOW_BANDWIDTH_MAIL = "The is a Tor Weather Report.\n\n"+\
 
 _T_SHIRT_SUBJ = 'Congratulations! Have a T-shirt!'
 _T_SHIRT_MAIL = "This is a Tor Weather Report.\n\n"+\
-    "Congratulations! The node you are observing has been %s for %s "+\
+    "Congratulations! The node you've been observing, %s, has been %s for %s "+\
     "days with an average bandwidth of %s KB/s," +\
     "which makes the operator eligible to receive an official Tor "+\
     "T-shirt! If you're interested in claiming your shirt, please visit "+\
@@ -110,7 +108,7 @@ _T_SHIRT_MAIL = "This is a Tor Weather Report.\n\n"+\
 
 _WELCOME_SUBJ = 'Welcome to Tor!'
 _WELCOME_MAIL = "Hello and welcome to Tor!\n\n" +\
-    "We've noticed that your Tor node %s(id = %s) has been running long "+\
+    "We've noticed that your Tor node %s has been running long "+\
     "enough to be "+\
     "flagged as \"stable\". First, we would like to thank you for your "+\
     "contribution to the Tor network! As Tor grows, we require ever more "+\
@@ -128,15 +126,25 @@ _WELCOME_MAIL = "Hello and welcome to Tor!\n\n" +\
     "critical security updates. To join, send an e-mail message to "+\
     "majordomo@seul.org "+\
     "with no subject and a body of \"subscribe or-announce\". \n\n"+\
-    "Thank you again for your contribution to the Tor network! "+\
+    "%sThank you again for your contribution to the Tor network! "+\
     "We won't send you any further emails unless you subscribe.\n\n"+\
-    "%sDisclaimer: If you have no idea why you're receiving this email, we "+\
+    "Disclaimer: If you have no idea why you're receiving this email, we "+\
     "sincerely apologize! You shouldn't hear from us again.\n"
 
-# ------------ FORMAT LINK IN WELCOME MAIL --------------------------
+_LEGAL_INFO = "Additionally, since you are running as an exit node, you " +\
+    "might be interested in Tor's Legal FAQ for Relay Operators "+\
+    "(http://www.torproject.org/eff/tor-legal-faq.html.en) " +\
+    "and Mike Perry's blog post on running an exit node " +\
+    "(http://blog.torproject.org/blog/tips-running-exit-node-minimal-"+\
+    "harassment).\n\n"
 
-# -------------- WRITE THIS!!!!! --------------------------------------
-_LEGAL_INFO = "Legal stuff!! \n\n"
+def _get_router_name(fingerprint):
+    """"""
+    router = Router.objects.get(fingerprint = fingerprint)
+    name = "(id: " + fingerprint + ")" 
+    if router.name != "Unnamed":
+        name = router.name + " ," + name
+    return name
 
 def send_confirmation(recipient,
                       fingerprint,
@@ -155,8 +163,9 @@ def send_confirmation(recipient,
     @type confirm_auth: str
     @param confirm_auth: The user's unique confirmation authorization key.
     """
+    name = _get_router_name(fingerprint)
     confirm_url = url_helper.get_confirm_url(confirm_auth)
-    msg = _CONFIRMATION_MAIL % (fingerprint, confirm_url)
+    msg = _CONFIRMATION_MAIL % (name, confirm_url)
     sender = _SENDER
     subj = _SUBJECT_HEADER + _CONFIRMATION_SUBJ
     send_mail(subj, msg, sender, [recipient], fail_silently=True)
@@ -179,19 +188,22 @@ def send_confirmed(recipient,
     @type pref_auth: str
     @param pref_auth: The user's unique preferences auth key
     """
+    name = _get_router_name(fingerprint)
     subj = _SUBJECT_HEADER + _CONFIRMED_SUBJ
     sender = _SENDER
     unsubURL = url_helper.get_unsubscribe_url(unsubs_auth)
     prefURL = url_helper.get_preferences_url(pref_auth)
-    msg = _CONFIRMED_MAIL % (fingerprint, unsubURL, prefURL) 
+    msg = _CONFIRMED_MAIL % (name, unsubURL, prefURL) 
     send_mail(subj, msg, sender, [recipient], fail_silently=False)
 
 def bandwidth_tuple(recipient, fingerprint, grace_pd, unsubs_auth, pref_auth):
+    """"""
+    name = _get_router_name(fingerprint)
     subj = _SUBJECT_HEADER + LOW_BANDWIDTH_SUBJ
     sender = _SENDER
     unsubURL = url_helper.get_unsubscribe_url(unsubs_auth)
     prefURL = url_helper.get_preferences_url(pref_auth)
-    msg = _LOW_BANDWIDTH_MAIL % (fingerprint, unsubURL, prefURL)
+    msg = _LOW_BANDWIDTH_MAIL % (name, unsubURL, prefURL)
 
     return (subj, msg, sender, [recipient])
 
@@ -212,24 +224,27 @@ def node_down_tuple(recipient, fingerprint, grace_pd, unsubs_auth, pref_auth):
     @return: A tuple listing information about the email to be sent, which is
         used by the send_mass_mail method in updaters.
     """
+    name = _get_router_name(fingerprint)
     subj = _SUBJECT_HEADER + _NODE_DOWN_SUBJ
     sender = _SENDER
     unsubURL = url_helper.get_unsubscribe_url(unsubs_auth)
     prefURL = url_helper.get_preferences_url(pref_auth)
-    msg = _NODE_DOWN_MAIL % (fingerprint, grace_pd, unsubURL,   
-                                     prefURL)
+    msg = _NODE_DOWN_MAIL % (name, grace_pd, unsubURL, prefURL)
     return (subj, msg, sender, [recipient])
 
 def t_shirt_tuple(recipient,
-                 avg_bandwidth,
-                 hours_since_triggered,
-                 is_exit,
-                 unsubs_auth,
-                 pref_auth):
+                  fingerprint,
+                  avg_bandwidth,
+                  hours_since_triggered,
+                  is_exit,
+                  unsubs_auth,
+                  pref_auth):
     """Returns a tuple for a t-shirt earned email.
     
     @type recipient: str
     @param recipient: The user's email address
+    @type fingerprint: str
+    @param fingerprint: The router's fingerprint
     @type avg_bandwidth: int
     @param avg_bandwidth: The user's average bandwidth over the
         observed period.
@@ -246,6 +261,7 @@ def t_shirt_tuple(recipient,
     @return: A tuple listing information about the email to be sent, which is
         used by the send_mass_mail method in updaters.
     """
+    name = _get_router_name(fingerprint)
     stable_message = 'running'
     if is_exit:
         node_type += ' as an exit node'
@@ -255,7 +271,7 @@ def t_shirt_tuple(recipient,
     sender = _SENDER
     unsubURL = url_helper.get_unsubscribe_url(unsubs_auth)
     prefURL = url_helper.get_preferences_url(pref_auth)
-    msg = _T_SHIRT_MAIL % (stable_message, days_running, 
+    msg = _T_SHIRT_MAIL % (name, stable_message, days_running, 
                                    avg_bandwidth, unsubURL, prefURL)
     return (subj, msg, sender, [recipient])
 
@@ -274,18 +290,14 @@ def welcome_tuple(recipient, fingerprint, exit):
     @return: A tuple listing information about the email to be sent, which is
         used by the send_mass_mail method in updaters.
     """
-    router = Router.objects.get(fingerprint=fingerprint)
-    router_name = router.name
-    name = ''
-    if router_name != 'Unnamed':
-        name = router_name + ', ' 
+    name = _get_router_name(fingerprint)
     subj = _SUBJECT_HEADER + _WELCOME_SUBJ
     sender = _SENDER
     append = ''
     # if the router is an exit node, append legal info 
     if exit:
         append = _LEGAL_INFO
-    msg = _WELCOME_MAIL % (name, router.fingerprint, append)
+    msg = _WELCOME_MAIL % (name, append)
     return (subj, msg, sender, [recipient])
 
 def version_tuple(recipient, fingerprint, unsubs_auth, pref_auth):
@@ -307,11 +319,11 @@ def version_tuple(recipient, fingerprint, unsubs_auth, pref_auth):
              an appropriate format for the C{send_mass_mail()} function in
              C{updaters}.
     """
-    router = Router.objects.get(fingerprint=fingerprint)
+    name = _get_router_name(fingerprint)
     subj = _SUBJECT_HEADER + _VERSION_SUBJ
     sender = _SENDER
     unsubURL = url_helper.get_unsubscribe_url(unsubs_auth)
     prefURL = url_helper.get_preferences_url(pref_auth)
     downloadURL = url_helper.get_download_url()
-    msg = _VERSION_MAIL % (fingerprint, downloadURL, unsubURL, prefURL)
+    msg = _VERSION_MAIL % (name, downloadURL, unsubURL, prefURL)
     return (subj, msg, sender, [recipient])
