29f7f2b4b3e33b2a7237cab848434ad3262b2ea7
Katie Wagner <kwagner@wesleyan.edu>
2010-07-07 11:44:56 -0400

    Scanning the code and removing unnecessary comment lines

--- weather/weatherapp/management.py
+++
@@ -1,9 +0,0 @@
-from django.dispatch import dispatcher
-from django.db.models import signals
-from weather.weatherapp import models
-import subprocess
-
-#def init_poller():
-    #subprocess.Popen(['python', 'poller.py'])
-
-#dispatcher.connect(init_poller, sender=models, signal=signals.post_syncdb)
