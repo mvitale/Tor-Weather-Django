1c12c61b7fbf2a6a81d3e34aeb7508faa389c6f9
Michael Vitale <mmvitale@gmail.com>
2010-07-16 16:46:58 -0400

    Fixed more issues from Prof. Danner's notes

--- weather/config/config.py
+++ weather/config/config.py
@@ -8,3 +8,6 @@ authenticator = open("config/auth_token", "r").read().strip()
 
 #The Tor control port for listener to use. Must be configured in your torrc file
 listener_port = '9055'
+
+#The Tor control port for updaters to use. Must be configured in your torrc file
+updater_port = '9056'
