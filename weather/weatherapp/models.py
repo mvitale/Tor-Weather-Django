71054d976656b2e341344ba292424ff3b4add947
Michael Vitale <mmvitale@gmail.com>
2010-07-22 14:30:43 -0400

    Made DeploymentDatetime model

--- weather/weatherapp/models.py
+++ weather/weatherapp/models.py
@@ -619,8 +619,8 @@ class PrefixedIntegerField(forms.IntegerField):
     @type _PREFIX_DEFAULT: C{str}
     @cvar _PREFIX_DEFAULT: Default prefix.
     @type _DEFAULT_ERRORS: C{dict} {C{str}: C{str}}
-    @cvar _DEFAULT_ERRORS: Dictionary mapping default error names to their error
-        messages.
+    @cvar _DEFAULT_ERRORS: Dictionary mapping default error names to their 
+    error messages.
 
     @type prefix: C{str}
     @ivar prefix: Prefix to use for this L{PrefixedIntegerField} instance. 
@@ -1341,3 +1341,17 @@ class PreferencesForm(GenericForm):
         elif new_data['get_t_shirt']:
             t = TShirtSub(subscriber=self.user)
             t.save()
+
+class DeploymentDatetime(models.Model):
+    """Stores the date and time when this instance of Tor Weather was first
+    deployed. This should only ever have one row, and is used by updaters to 
+    populate the router table for 48 hours after deployment without sending
+    welcome emails.
+
+    @type deployed: DateTimeField (datetime)
+    @ivar deployed: The datetime that this instance of Tor Weather was first
+    deployed.
+    """
+
+    deployed = models.DateTimeField()
+
