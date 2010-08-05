ec74f2e6aa153205826b30dc66426be1b6064f85
Katie Wagner <kwagner@wesleyan.edu>
2010-08-05 16:29:34 -0400

    Email strings are no longer private

--- weather/weatherapp/emails.py
+++ weather/weatherapp/emails.py
@@ -1,52 +1,52 @@
 """The emails module contains methods to send individual confirmation and confirmed emails as well as methods to return tuples needed by Django's 
 send_mass_mail() method. Emails are sent after all database checks/updates. 
 
-@type _SENDER: str
-@var _SENDER: The email address for the Tor Weather emailer
-@type _SUBJECT_HEADER: str
-@var _SUBJECT_HEADER: The base for all email headers.
-@type _CONFIRMATION_SUBJ: str
-@var _CONFIRMATION_SUBJ: The subject line for the confirmation mail
-@type _CONFIRMATION_MAIL: str
-@var _CONFIRMATION_MAIL: The email message sent upon first 
+@type SENDER: str
+@var SENDER: The email address for the Tor Weather emailer
+@type SUBJECT_HEADER: str
+@var SUBJECT_HEADER: The base for all email headers.
+@type CONFIRMATION_SUBJ: str
+@var CONFIRMATION_SUBJ: The subject line for the confirmation mail
+@type CONFIRMATION_MAIL: str
+@var CONFIRMATION_MAIL: The email message sent upon first 
     subscribing. The email contains a link to the user-specific confirmation
     page, which the user must follow to confirm.
-@type _CONFIRMED_SUBJ: str
-@var _CONFIRMED_SUBJ: The subject line for the confirmed email
-@type _CONFIRMED_MAIL: str
-@var _CONFIRMED_MAIL: The email message sent after the user follows the 
+@type CONFIRMED_SUBJ: str
+@var CONFIRMED_SUBJ: The subject line for the confirmed email
+@type CONFIRMED_MAIL: str
+@var CONFIRMED_MAIL: The email message sent after the user follows the 
     link in the confirmation email. Contains links to preferences and 
     unsubscribe.
-@type _NODE_DOWN_SUBJ: str
-@var _NODE_DOWN_SUBJ: The subject line for the node down notification
-@type _NODE_DOWN_MAIL: str
-@var _NODE_DOWN_MAIL: The email message for the node down notification.
-@type _VERSION_SUBJ: str
-@var _VERSION_SUBJ: The subject line for an out-of-date version notification
-@type _VERSION_MAIL: str
-@var _VERSION_MAIL: The email message for an out-of-date version 
+@type NODE_DOWN_SUBJ: str
+@var NODE_DOWN_SUBJ: The subject line for the node down notification
+@type NODE_DOWN_MAIL: str
+@var NODE_DOWN_MAIL: The email message for the node down notification.
+@type VERSION_SUBJ: str
+@var VERSION_SUBJ: The subject line for an out-of-date version notification
+@type VERSION_MAIL: str
+@var VERSION_MAIL: The email message for an out-of-date version 
     notification
-@type _LOW_BANDWIDTH_SUBJ: str
-@var _LOW_BANDWIDTH_SUBJ: The subject line for the low bandwidth notification. 
-@type _LOW_BANDWIDTH_MAIL: str
-@var _LOW_BANDWIDTH_MAIL: The email message for the low bandwidth notification.
-@type _T_SHIRT_SUBJ: str
-@var _T_SHIRT_SUBJ: The subject line for the T-Shirt notification
-@type _T_SHIRT_MAIL: str
-@var _T_SHIRT_MAIL: The email message for the T-Shirt notification (includes 
+@type LOW_BANDWIDTH_SUBJ: str
+@var LOW_BANDWIDTH_SUBJ: The subject line for the low bandwidth notification. 
+@type LOW_BANDWIDTH_MAIL: str
+@var LOW_BANDWIDTH_MAIL: The email message for the low bandwidth notification.
+@type T_SHIRT_SUBJ: str
+@var T_SHIRT_SUBJ: The subject line for the T-Shirt notification
+@type T_SHIRT_MAIL: str
+@var T_SHIRT_MAIL: The email message for the T-Shirt notification (includes 
     uptime and avg bandwidth over that period)
-@type _WELCOME_SUBJ: str
-@var _WELCOME_SUBJ: The subject line for the welcome email
-@type _WELCOME_MAIL: str
-@var _WELCOME_MAIL: The message in the welcome email. This message is sent to
+@type WELCOME_SUBJ: str
+@var WELCOME_SUBJ: The subject line for the welcome email
+@type WELCOME_MAIL: str
+@var WELCOME_MAIL: The message in the welcome email. This message is sent to
     all node operators running a stable node (assuming we can parse their 
     email). It welcomes the user to Tor, describes Tor Weather, and provides 
     legal information if the user is running an exit node.
-@type _LEGAL_INFO: str
-@var _LEGAL_INFO: Legal information to assist exit node operators. This is 
+@type LEGAL_INFO: str
+@var LEGAL_INFO: Legal information to assist exit node operators. This is 
     appended to the welcome email if the recipient runs an exit node.
-@type _GENERIC_FOOTER: str
-@var _GENERIC_FOOTER: A footer containing unsubscribe and preferences page
+@type GENERIC_FOOTER: str
+@var GENERIC_FOOTER: A footer containing unsubscribe and preferences page
     links.
 """
 import re
@@ -56,11 +56,11 @@ from weatherapp.models import insert_fingerprint_spaces
 
 from django.core.mail import send_mail
 
-_SENDER = 'tor-ops@torproject.org'
-_SUBJECT_HEADER = '[Tor Weather] '
+SENDER = 'tor-ops@torproject.org'
+SUBJECT_HEADER = '[Tor Weather] '
 
-_CONFIRMATION_SUBJ = 'Confirmation Needed'
-_CONFIRMATION_MAIL = "Dear human,\n\n" +\
+CONFIRMATION_SUBJ = 'Confirmation Needed'
+CONFIRMATION_MAIL = "Dear human,\n\n" +\
     "This is the Tor Weather Report system.\n\n" +\
     "Someone (possibly you) has requested that status monitoring "+\
     "information about a Tor node %s be sent to this email "+\
@@ -69,32 +69,32 @@ _CONFIRMATION_MAIL = "Dear human,\n\n" +\
     "Reports, you don't need to do anything. You shouldn't hear from us "+\
     "again."
 
-_CONFIRMED_SUBJ = 'Confirmation Successful'
-_CONFIRMED_MAIL="Dear human,\n\nThis is the Tor Weather Report "+\
+CONFIRMED_SUBJ = 'Confirmation Successful'
+CONFIRMED_MAIL="Dear human,\n\nThis is the Tor Weather Report "+\
     "system.You successfully subscribed for Weather Reports about a Tor "+\
     "node %s."
 
-_NODE_DOWN_SUBJ = 'Node Down!'
-_NODE_DOWN_MAIL = "This is a Tor Weather Report.\n\n" +\
+NODE_DOWN_SUBJ = 'Node Down!'
+NODE_DOWN_MAIL = "This is a Tor Weather Report.\n\n" +\
     "It appears that the node %s you've been observing" +\
     "has been uncontactable through the Tor network for at least %s. "+\
     "You may wish to look at it to see why."
 
-_VERSION_SUBJ = 'Node Out of Date!'
-_VERSION_MAIL = "This is a Tor Weather Report.\n\n"+\
+VERSION_SUBJ = 'Node Out of Date!'
+VERSION_MAIL = "This is a Tor Weather Report.\n\n"+\
     "It appears that the Tor node %s you've been observing "+\
     "is running an %s version of Tor. You can download the "+\
     "latest version of Tor at %s."
 
-_LOW_BANDWIDTH_SUBJ = 'Low bandwidth!'
-_LOW_BANDWIDTH_MAIL = "The is a Tor Weather Report.\n\n"+\
+LOW_BANDWIDTH_SUBJ = 'Low bandwidth!'
+LOW_BANDWIDTH_MAIL = "The is a Tor Weather Report.\n\n"+\
     "It appears that the tor node %s you've been observing "+\
     "has an observed bandwidth capacity of %s kB/s. You elected to receive "+\
     "notifications if this node's bandwidth capacity passed a threshold of "+\
     "%s kB/s. You may wish to look at your router to see why."
 
-_T_SHIRT_SUBJ = 'Congratulations! Have a T-shirt!'
-_T_SHIRT_MAIL = "This is a Tor Weather Report.\n\n"+\
+T_SHIRT_SUBJ = 'Congratulations! Have a T-shirt!'
+T_SHIRT_MAIL = "This is a Tor Weather Report.\n\n"+\
     "Congratulations! The node %s you've been observing has been %s for %s "+\
     "days with an average bandwidth of %s KB/s," +\
     "which makes the operator eligible to receive an official Tor "+\
@@ -104,8 +104,8 @@ _T_SHIRT_MAIL = "This is a Tor Weather Report.\n\n"+\
     "\n\nYou might want to include this message in your email. "+\
     "Thank you for your contribution to the Tor network!"
     
-_WELCOME_SUBJ = 'Welcome to Tor!'
-_WELCOME_MAIL = "Hello and welcome to Tor!\n\n" +\
+WELCOME_SUBJ = 'Welcome to Tor!'
+WELCOME_MAIL = "Hello and welcome to Tor!\n\n" +\
     "We've noticed that your Tor node %s has been running long "+\
     "enough to be "+\
     "flagged as \"stable\". First, we would like to thank you for your "+\
@@ -129,38 +129,17 @@ _WELCOME_MAIL = "Hello and welcome to Tor!\n\n" +\
     "Disclaimer: If you have no idea why you're receiving this email, we "+\
     "sincerely apologize! You shouldn't hear from us again."
 
-_LEGAL_INFO = "Additionally, since you are running as an exit node, you " +\
+LEGAL_INFO = "Additionally, since you are running as an exit node, you " +\
     "might be interested in Tor's Legal FAQ for Relay Operators "+\
     "(http://www.torproject.org/eff/tor-legal-faq.html.en) " +\
     "and Mike Perry's blog post on running an exit node " +\
     "(http://blog.torproject.org/blog/tips-running-exit-node-minimal-"+\
     "harassment).\n\n"
 
-_GENERIC_FOOTER = "\n\nYou can unsubscribe from these reports at any time "+\
+GENERIC_FOOTER = "\n\nYou can unsubscribe from these reports at any time "+\
     "by visiting the following url:\n\n%s\n\nor change your Tor Weather "+\
     "notification preferences here: \n\n%s"
 
-
-def _get_router_name(fingerprint, name):
-    """Returns a string representation of the name and fingerprint of
-    this router. Ex: 'WesCSTor (id: 4094 8034 ...)'
-    
-    @type fingerprint: str
-    @param fingerprint: A router fingerprint
-    @type name: str
-    @param name: A router name
-
-    @rtype: str
-    @return: An email-friendly string representation of the name and
-    fingerprint. Only returns a representation of the fingerprint 
-    C{if name == 'Unnamed'}.
-    """
-
-    spaced_fingerprint = insert_fingerprint_spaces(fingerprint) 
-    if name == 'Unnamed':
-        return "(id: %s)" % spaced_fingerprint
-    else:
-        return "%s (id: %s)" % (name, spaced_fingerprint)
 
 def send_confirmation(recipient, fingerprint, name, confirm_auth):
     """This method sends a confirmation email to the user. The email 
